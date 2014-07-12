a305a7c377ca0713c7d9db71621f6c8263fc1775
qk831019 <qian450@gmail.com>
2014-07-11 23:14:51 -0700

    new file

---
+++ best.R
@@ -0,0 +1,10 @@
+best <- function(state, outcome) {
+  ## Read outcome data
+  ## Check that state and outcome are valid
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+  outcome = read.csv('outcome-of-care-measures.csv',colClasses ='character')
+  outcome[, 11] <- as.numeric(outcome[, 11])
+  
+  
+}
